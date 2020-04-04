d6e398a0dc24f374ad885b29f5fbc3e793fcb3d8
NoNo <nonoesimposible72@gmail.com>
2020-04-04 14:58:01 -0600

    Rewrite the system so that every app has its explicit instructions

--- ignition/Main.py
+++ ignition/Main.py
@@ -10,7 +10,6 @@ from gi.repository import Gtk
 import sys
 
 from app_categories import categories
-from Install import *
 
 class HomeWindow(Gtk.ApplicationWindow):
     def __init__(self):
@@ -18,14 +17,10 @@ class HomeWindow(Gtk.ApplicationWindow):
         # system("mkdir ~/.local/share/ignition/")
         # self.appdata = home+"/.local/share/ignition/"
 
-        installer = Install()
-
         tabs = Gtk.Notebook.new()
 
         prevbutton = []
         for category in categories:
-            name = category.name
-            tabgrid = name
             tabgrid = Gtk.Grid.new()
 
             # print(gridlist)
@@ -35,8 +30,8 @@ class HomeWindow(Gtk.ApplicationWindow):
 
             for item in category:
                 button = Gtk.Button.new() 
-                button.set_label(item)
-                # button.connect("clicked", installer.allitemmethod)
+                button.set_label(item.name)
+                button.connect("clicked", self.install, item)
                 if col == 0:
                     # print("Attach")
                     tabgrid.attach(button, col, row, 2, 1)
@@ -52,13 +47,21 @@ class HomeWindow(Gtk.ApplicationWindow):
                 else:
                     col = col + 1
 
-            tabs.insert_page(tabgrid, Gtk.Label.new(name), -1)
+            tabs.insert_page(tabgrid, Gtk.Label.new(category.name), -1)
 
         self.add(tabs)
 
-    # def install(self, button, name):
-    #     print("Install called")
+    # Currently button is not used. I need to look up Python syntax to make that cleaner.
+    def install(self, button, item):
 
+        if item.pkg_manager == "apt":
+            print(f"pkexec apt-get --yes install {item.pkg_name} && exit")
+            # system("pkexec apt-get --yes install " + pkg_name + " && exit")
+        elif item.pkg_manager == "snap":
+            print(f"pkexec snap install {item.pkg_name} && exit")
+            # system("pkexec snap install " + pkg_name + " && exit")
+        else:
+            print(f"Something myseterious went wrong when trying to install {item.name}.")
 
 win = HomeWindow()
 
--- ignition/app_categories.py
+++ ignition/app_categories.py
@@ -6,6 +6,19 @@ or remove an app from a category.
 We have two methods of categorization: by package manager, or by category.
 """
 
+class App:
+    def __init__(self, name, pkg_name, pkg_manager):
+        """
+        name        :   human-readable, capitalized name
+        pkg_name    :   name of the package used by the package manager (can be space-separated to install multiple packages if all the packages use the same package manager and the package manager allows it)
+        pkg_manager :   package manager to use (currently supported is apt, snap
+        """
+
+        self.name = name
+        self.pkg_name = pkg_name
+        self.pkg_manager = pkg_manager
+
+
 class Category(list):
     """ Almost the exact same thing as a list, but with an additional .name attribute. """
     def __init__(self, name, apps):
@@ -13,189 +26,88 @@ class Category(list):
         self.name = name
 
 
-apps_by_pkg_manager = {
-    "aptitude": [
-        "neofetch",
-        "redshift",
-        "baobab",
-        "wine",
-        "safeeyes",
-        "steamengine-locomotive",
-        "gdebi",
-        "gnome-tweaks",
-        "virtualbox",
-        "firefox",
-        "synapse",
-        "plank",
-        "cario",
-        "chromium",
-        "thunderbird",
-        "google-earth",
-        "gnome-games",
-        "gnome-breakout",
-        "gnome-games-app",
-        "libreoffice",
-        "abiword",
-        "gnumeric",
-        "papirus-icon-theme",
-        "pocillo-icon-theme",
-        "faenza-icon-theme",
-        "xscreensaver",
-        "vlc",
-        "rhythmbox",
-        "pulseaudio",
-        "gstreamer",
-        "adobe-flash-player",
-        "gnome-image-viewer",
-        "totem",
-        "bb",
-        "aa",
-        "tilix",
-        "gnome-terminal",
-        "xfce-terminal",
-        "lxterminal",
-        "xterm",
-        "mate-terminal",
-        "konsole",
-        "git",
-        "gedit",
-        "mousepad",
-        "openshot",
-        "ubuntustudio"
-    ],
-    "snapd": [
-        "chromium",
-        "mailspring",
-        "midori",
-        "googletools",
-        "yakyak",
-        "ggmail",
-        "supertuxkart",
-        "openofficedesktopeditors",
-        "micropad",
-        "p3x-onenote",
-        "projectlibre",
-        "spotify",
-        "code",
-        "atom",
-        "sublime",
-        "androidstudio",
-        "eclipse",
-        "gimp",
-        "blender",
-        "inkscape",
-        "kdenlive",
-        "krita"
-    ],
-    "gdebi": [
-        "ulauncher",
-        "insync3",
-        "steam",
-        "minecraft",
-        "apacheopenoffice"
-    ],
-    "custom": [
-        "pling"
-    ]
-}
-
 categories = [
     Category("Basics", [
-        "All Basics",
-        "Neofetch",
-        "Gdebi",
-        "Redshift",
-        "Gnome Tweaks",
-        "Baobab",
-        "WINE",
-        "Safe Eyes",
-        "Steamengine Locomotive"
+        App("Neofetch", "neofetch", "apt"),
+        App("Gdebi", "gdebi", "apt"),
+        App("Redshift", "redshift", "apt"),
+        App("GNOME Tweaks", "gnome-tweaks", "apt"),
+        App("Baobab", "baobab", "apt"),
+        App("WINE", "wine", "apt"),
+        App("Safe Eyes", "safeeyes", "apt")
     ]),
     Category("Launchers", [
-        "Synapse",
-        "Plank",
-        "ULauncher",
-        "Cario"
+        App("Synapse", "synapse", "apt"),
+        App("Plank", "plank", "apt"),
+        App("Cairo", "cairo-dock", "apt")
     ]),
     Category("Browsers", [
-        "Chromium",
-        "Chromium on APT",
-        "Firefox",
-        "Midori (Very Light)"
+        App("Chromium (snap)", "chromium", "snap"),
+        App("Chromium (apt)", "chromium-browser", "apt"), # already a transitional package to Snap
+        App("Firefox", "firefox", "apt"),
+        App("Midori (very light)", "midori", "snap")
     ]),
     Category("Cloud", [
-        "Google Tools",
-        "YakYak",
-        "Gnome Gmail"
+        App("YakYak", "yakyak", "snap"),
     ]),
     Category("Games", [
-        "Gnome Games App",
-        "Steam",
-        "Minecraft",
-        "Gnome Games Suite",
-        "Super Tux Kart",
-        "Gnome Breakout"
+        App("GNOME Games App", "gnome-games-app", "apt"),
+        App("GNOME Games Suite", "gnome-games", "apt"),
+        App("Steam", "steam", "apt"), # used to be via Gdebi, is this a problem?
+        App("Super Tux Kart", "supertuxkart", "snap"),
+        App("GNOME Breakout", "gnome-breakout", "apt")
     ]),
     Category("Office", [
-        "Libreoffice",
-        "OPENOFFICE Desktop Editors",
-        "Abiword",
-        "Gnumeric",
-        "Google Tools",
-        "microPad",
-        "P3X Onenote",
-        "Apache OpenOffice",
-        "ProjectLibre"
+        App("Libreoffice Suite", "libreoffice-common libreoffice-writer libreoffice-impress libreoffice-base libreoffice-calc libreoffice-math libreoffice-draw", "apt"),
+        App("Abiword", "abiword", "apt"),
+        App("Gnumeric", "gnumeric", "apt"),
+        App("μPad", "micropad", "snap"),
+        App("P3X Onenote", "p3x-onenote", "snap")
     ]),
     Category("Themes", [
-        "Papirus Icon Theme",
-        "Pocillo Icon Theme",
-        "Faenza Icon Theme",
-        "Pling Store",
-        "XScreenSaver"
+        App("Papirus Icon Theme", "papirus-icon-theme", "apt"),
+        App("Pocillo Icon Theme", "pocillo-icon-theme", "apt"),
+        App("Faenza Icon Theme", "faenza-icon-theme", "apt"),
+        App("XScreenSaver", "xscreensaver", "apt")
     ]),
     Category("Media", [
-        "VLC Media Player",
-        "Rhythmbox",
-        "Spotify",
-        "PAVU Control",
-        "GStreamer Codecs",
-        "Gnome Image Viewer",
-        "Totem",
-        "Flash Player"
+        App("VLC Media Player", "vlc", "apt"), # originally also was supposed to include DVD (.VOB) support
+        App("Rhythmbox", "rhythmbox", "apt"),
+        App("Spotify", "spotify", "snap"),
+        App("PAVU Control", "pavucontrol", "apt"),
+        App("GNOME Image Viewer", "eog", "apt"),
+        App("GNOME Videos", "totem", "apt")
     ]),
     Category("Toys", [
-        "BB",
-        "AA Lib"
+        App("BB", "bb", "apt"),
     ]),
     Category("Terminals", [
-        "Tilix",
-        "Terminator",
-        "Gnome Terminal",
-        "Xfce Terminal",
-        "LXTerminal",
-        "XTerm",
-        "MATE Terminal",
-        "Konsole"
+        App("Tilix", "tilix", "apt"),
+        App("Terminator", "terminator", "apt"),
+        App("GNOME Terminal", "gnome-terminal", "apt"),
+        App("Xfce Terminal", "xfce4-terminal", "apt"),
+        App("LXTerminal", "lxterminal", "apt"),
+        App("XTerm", "xterm", "apt"),
+        App("MATE Terminal", "mate-terminal", "apt"),
+        App("Konsole", "konsole", "apt")
     ]),
     Category("Programming", [
-        "GIT SCM",
-        "VS Code",
-        "Atom",
-        "Sublime",
-        "Android Studio",
-        "Eclipse",
-        "Gedit",
-        "Mousepad"
+        App("Git", "git", "apt"),
+        App("VS Code", "code", "snap"), # Do we need a --classic here?
+        App("Atom", "atom", "snap"),
+        App("Sublime", "sublime-text", "snap"),
+        App("Android Studio", "android-studio", "snap"),
+        App("Eclipse", "eclipse", "snap"),
+        App("Gedit", "gedit", "apt"),
+        App("Mousepad", "mousepad", "apt")
     ]),
     Category("Media Production", [
-        "GIMP Photo Editor",
-        "Blender",
-        "Inkscape",
-        "Open Shot",
-        "Kdenlive",
-        "Krita",
-        "Ubuntu Studio"
+        App("GIMP Photo Editor", "gimp", "snap"),
+        App("Blender", "blender", "snap"),
+        App("Inkscape", "inkscape", "snap"),
+        App("OpenShot", "openshot", "apt"),
+        App("Kdenlive", "kdenlive", "snap"),
+        App("Krita", "krita", "snap")
     ])
 ]
 
